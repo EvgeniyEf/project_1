19b8a391d4f357b22812ab19827660f3db3283a1
EvgeniyEf <efimov.evgeny1@gmail.com>
2022-04-06 13:32:45 +0300

    project_1

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//const result = confirm('are you here?');
+//console.log(result);
+
+const answers = [];
+
+//answers[0] = prompt('как ваше имя?', '');
+//answers[1] = prompt('как ваше фамилия?', '');
+//answers[2] = prompt('сколько вам лет?', '');
+
+//document.write(answers);
